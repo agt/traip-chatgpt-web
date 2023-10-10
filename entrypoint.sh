f77c5aaa848668857b239d5c8c8c43e5a1a17b50
Adam Tilghman <agt@ucsd.edu>
2023-10-09 21:30:36 -0700

    change default model

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/ash
 
 SRV_PORT=${SRV_PORT:-8000}
+MODEL=${MODEL:-llama-2}
 
 VITE_API_BASE=${OPENAI_API_BASE:-https://its-llamarama.ucsd.edu/}
 VITE_ENDPOINT_COMPLETIONS=${OPENAI_ENDPOINT_COMPLETIONS:-/v1/chat/completions}
@@ -12,6 +13,8 @@ cat /dev/null > .env
 echo "VITE_API_BASE=${VITE_API_BASE}" >> .env
 echo "VITE_ENDPOINT_COMPLETIONS=${VITE_ENDPOINT_COMPLETIONS}" >> .env
 echo "VITE_ENDPOINT_MODELS=${VITE_ENDPOINT_MODELS}" >> .env
+
+sed -i -e "s#gpt-3.5-turbo#${MODEL}#" ./src/lib/Settings.svelte
 
 npm run build:github
 ln -s . dist/chatgpt-web
